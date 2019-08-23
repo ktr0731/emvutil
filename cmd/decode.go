b9b8921390a31209068923b59198ced4867379b0
ktr0731 <ktr@syfm.me>
2019-08-23 16:26:03 +0900

    add decode sub command

---
+++ cmd/decode.go
@@ -0,0 +1,98 @@
+/*
+Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/k0kubun/pp"
+	"github.com/spf13/cobra"
+	"go.mercari.io/go-emv-code/mpm"
+	"go.mercari.io/go-emv-code/mpm/jpqr"
+	"go.mercari.io/go-emv-code/tlv"
+)
+
+// decodeCmd represents the decode command
+var decodeCmd = &cobra.Command{
+	Use:   "decode",
+	Short: "decode input as a JPQR payload",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		var in io.Reader
+		if len(args) != 0 {
+			in = strings.NewReader(strings.Join(args, "\n"))
+		} else {
+			in = os.Stdin
+		}
+		var hasErr bool
+		s := bufio.NewScanner(in)
+		for s.Scan() {
+			if *decodeAsID {
+				code := &mpm.Code{
+					MerchantAccountInformation: []tlv.TLV{
+						{
+							Tag:    "26",
+							Length: "68",
+							Value:  s.Text(),
+						},
+					},
+				}
+				id, err := jpqr.ParseID(code)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "failed to decode id '%s'", s.Text())
+					hasErr = true
+					continue
+				}
+				pp.Println(id)
+			} else {
+				code, err := jpqr.Decode(s.Bytes())
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "failed to decode payload '%s'", s.Text())
+					hasErr = true
+					continue
+				}
+				pp.Println(code)
+			}
+		}
+		if hasErr {
+			os.Exit(1)
+		}
+	},
+}
+
+var (
+	decodeAsID *bool
+)
+
+func init() {
+	rootCmd.AddCommand(decodeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// decodeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// decodeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	decodeAsID = decodeCmd.Flags().Bool("id", false, "decode input as an JPQR ID")
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/ktr0731/emvutil
+
+go 1.12
+
+require (
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.4.0
+	go.mercari.io/go-emv-code v0.1.2
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+)
